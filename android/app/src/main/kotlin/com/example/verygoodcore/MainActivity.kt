b4c3de3340c6670ac5dc02a095870e6b7d47c4e1
mercz <mercz@hotmail.com>
2022-07-18 19:26:47 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.bloc01_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:bloc01_timer/app/app.dart';
+import 'package:bloc01_timer/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
